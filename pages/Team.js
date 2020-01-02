99de2c76fce6bc218790c61c53075f8a8d3cdc76
Devansh2308 <48081120+Devansh2308@users.noreply.github.com>
2020-01-02 10:36:13 +0530

    Team, index,chemira page added

---
+++ pages/Team.js
@@ -0,0 +1,40 @@
+
+import Head from 'next/head';
+
+import Nav2_Team from "../components/Nav2_Team";
+import Header_Team from "../components/Header_Team";
+import { MaterialGrid_Team } from "../components/MaterialGrid_Team";
+
+
+
+
+const index = () =>{
+    return(
+        <div>
+            <Head>
+                <title>ISTE MANIT TEAM</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                
+                {/* <link href="https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap" rel="stylesheet"></link> */}
+                <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet"></link>
+                {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" /> */}
+                <link href="https://fonts.googleapis.com/css?family=Roboto:300&display=swap" rel="stylesheet"></link> 
+                <link href="https://fonts.googleapis.com/css?family=Noto+Sans:600&display=swap" rel="stylesheet"></link>
+                <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet"></link>
+            </Head>
+           
+            
+            
+            
+            <Nav2_Team/>
+            <Header_Team/>
+            <MaterialGrid_Team />
+            
+            
+        </div>
+       
+ 
+    )
+}
+  
+  export default index;
---
+++ pages/index.js
@@ -0,0 +1,39 @@
+
+import Head from 'next/head';
+import Home from "../components/Home";
+import About from "../components/About";
+import Events1 from "../components/events1";
+import Nav2 from "../components/Nav2";
+import Contact from "../components/Contact";
+
+
+
+const index = () =>{
+    return(
+        <div>
+            <Head>
+                <title>ISTE MANIT</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                
+                {/* <link href="https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap" rel="stylesheet"></link> */}
+                <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet"></link>
+                {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" /> */}
+                <link href="https://fonts.googleapis.com/css?family=Roboto:300&display=swap" rel="stylesheet"></link> 
+                <link href="https://fonts.googleapis.com/css?family=Noto+Sans:600&display=swap" rel="stylesheet"></link>
+                <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet"></link>
+            </Head>
+           
+            
+            <Home/>
+            <About/>
+            <Events1/>
+            
+            <Nav2/>
+            <Contact/>
+        </div>
+       
+ 
+    )
+}
+  
+  export default index;
