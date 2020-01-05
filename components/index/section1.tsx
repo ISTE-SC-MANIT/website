b9cb2f5a0528af614d23014ac321fe36ebc46418
Your Name <you@example.com>
2020-01-05 18:20:04 +0530

    feat(section 2 , section 3 , section 4 were added): components added

--- components/index/section1.tsx
+++ components/index/section1.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Theme, Typography } from "@material-ui/core";
+import { Container, Theme, Typography, Grid, Paper } from "@material-ui/core";
+import { useMediaQuery } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme: Theme) => {
@@ -8,45 +9,114 @@ const useStyles = makeStyles((theme: Theme) => {
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            padding: "10rem",
+            padding: "1rem",
 
             [theme.breakpoints.down("sm")]: {
                 padding: "8rem"
             }
         },
         root: {
-            background: `linear-gradient(to right bottom, ${theme.palette.primary.dark}, ${theme.palette.primary.light})`
-            // height: `100vh`,
-            // "&::after": {
-            //     content: "' '",
-            //     display: "block",
-            //     borderWidth: 100,
-            //     borderStyle: "solid",
-            //     borderBottomColor: "white",
-            //     borderRightColor: "white",
-            //     borderTop: "transparent",
-            //     borderLeft: "transparent"
-            // }
+            backgroundImage: `url(${"/static/banner_bg.png"})`,
+            marginLeft: "-200px",
+            paddingBottom: "60px"
+            // marginTop: "-380px"
         },
         heading: {
-            color: theme.palette.getContrastText(theme.palette.primary.main)
+            color: "white",
+            marginLeft: "250px"
+        },
+        fp: {
+            paddingTop: "35rem",
+            paddingBottom: "15rem"
+        },
+        image1: {
+            width: "400px",
+            height: "300px",
+            position: "relative"
+        },
+        image2: {
+            width: "400px",
+            height: "300px",
+            position: "relative",
+            top: "-200px",
+            left: "50px"
+        },
+        image3: {
+            width: "400px",
+            height: "300px",
+            position: "relative",
+            top: "-400px",
+            left: "100px"
+        },
+        imgcontainer: {
+            marginTop: "50px",
+            background: "transparent",
+            border: "null",
+            marginLeft: "50px"
+        },
+        grid1: {
+            marginTop: "190px",
+            textAlign: "center"
+        },
+        [theme.breakpoints.down("sm")]: {
+            grid2: {
+                display: "none"
+            },
+            root: {
+                paddingBottom: "40vh",
+                marginTop: "00px",
+                marginLeft: "-400px"
+            },
+            heading: {
+                marginLeft: "400px",
+                padding: "10px"
+            }
         }
     };
 });
 
 const Section1: React.FC = () => {
     const classes = useStyles();
+
     return (
         <div className={classes.root}>
-            <Container maxWidth="md" className={classes.container}>
-                <Typography
-                    variant="h2"
-                    component="h1"
-                    className={classes.heading}
-                >
-                    ISTE SC MANIT
-                </Typography>
-            </Container>
+            <Grid container spacing={0}>
+                <Grid item xs={12} sm={6} lg={8} className={classes.grid1}>
+                    <Typography
+                        variant="h3"
+                        component="h2"
+                        className={classes.heading}
+                    >
+                        ISTE SC MANIT
+                    </Typography>
+                    <Typography variant="subtitle1" className={classes.heading}>
+                        The best Students,s Chapter from last 3 years in MP-CG
+                        region
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} sm={6} lg={4} className={classes.grid2}>
+                    <div className={classes.imgcontainer}>
+                        <div>
+                            <img
+                                src="/static/version.jpg"
+                                className={classes.image1}
+                            ></img>
+                        </div>
+                        <div>
+                            <img
+                                src="/static/chimerax.jpg"
+                                className={classes.image2}
+                            ></img>
+                        </div>
+                        <div>
+                            <img
+                                src="/static/codathon.jpg"
+                                className={classes.image3}
+                            ></img>
+                        </div>
+                    </div>
+                </Grid>
+            </Grid>
         </div>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import { NextPage } from "next";
 import React from "react";
 import Section1 from "../components/index/section1";
+import Section2 from "../components/index/section2";
+import Section3 from "../components/index/section3";
+import Section4 from "../components/index/section4";
 
 const Index: NextPage = () => {
-    return <Section1 />;
+    return (
+        <div>
+            <Section1 />
+            <Section2 />
+            <Section3 />
+            <Section4 />
+        </div>
+    );
 };
 
 export default Index;
