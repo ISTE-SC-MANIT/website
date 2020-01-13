3b661fdd0b77a615a02ce5f73e706006c6bf30d9
Gourav raj <gouravrj9654@gmail.com>
2020-01-13 17:57:37 +0530

    feat: changes in theme and other responsiveness

--- components/index/theme.ts
+++ components/index/theme.ts
@@ -1,10 +1,10 @@
 import { createMuiTheme } from "@material-ui/core";
-import { blue, lightBlue } from "@material-ui/core/colors";
+import { blue, lightBlue,orange } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: lightBlue[`A400`]
+            main:orange['400']
         }
     }
 });
